2d1459223477b37c2f49422f2de6c48d8d2ff504
mateus <mateus@RZN>
2023-09-07 19:09:35 -0300

    Add project files.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,37 @@
+﻿class Musica
+{
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public Banda Artista {  get; }
+    public int Duracao {  get; set; }
+    public bool Disponivel {  get; set; }
+    public Genero Genero { get; set; }
+    //narrow function ou LAMBDA
+    public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
+
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+
+        if(Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+
+        }else {
+            Console.WriteLine("Adquira o plano Plus+");
+                }
+    }
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"Nome/Artista: {Nome} - {Artista}");
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,45 @@
+﻿//Banda queen = new Banda("Queen");
+
+//Album albumDoQueen = new Album("A Night at the Opera");
+
+//Musica musica1 = new Musica(queen, "Love of my Life")
+//{
+//    Duracao = 213,
+//    Disponivel = true,
+//};
+
+
+//Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+//{
+//    Duracao = 353,
+//    Disponivel = false,
+//};
+
+//albumDoQueen.AdicionarMusica(musica1);
+//albumDoQueen.AdicionarMusica(musica2);
+//queen.AdicionarAlbum(albumDoQueen);
+
+//musica1.ExibirFichaTecnica();
+//musica2.ExibirFichaTecnica();
+//albumDoQueen.ExibirMusicasDoAlbum();
+//queen.ExibirDiscografia();
+
+Episodio ep1 = new(1, "Space Today - Materia Escura", 45);
+ep1.AdicionarConvidados("Claudio Roberto");
+ep1.AdicionarConvidados("Marcos Pontes");
+
+Episodio ep2 = new(2, "Space Today - Estamos sozinhos?", 56);
+ep2.AdicionarConvidados("Neo");
+ep2.AdicionarConvidados("Fernando Marcelo");
+
+Episodio ep3 = new(3, "Space Today - Supercondutores", 48);
+ep3.AdicionarConvidados("Rogerinho");
+ep3.AdicionarConvidados("Luis Interessante");
+
+Podcast podcast = new("Ciência Sempre", "Serjao dos Foguetes");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.AdicionarEpisodio(ep3);
+podcast.ExibirDetalhes();
+
+
